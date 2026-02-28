1e1f2777dc246a46fb4c8b5247b0d43ad0bb37c1
shawly <shawlyde@gmail.com>
2026-02-28 11:11:53 +0100

    refactor(docker): streamline Dockerfile and entrypoint script
    Refactor the Dockerfile to improve readability by formatting the
    package installation commands across multiple lines. This enhances
    maintainability and clarity.
    
    Also, adjust the entrypoint script for consistent indentation and
    readability, ensuring that the GPG password handling logic is clear
    and easy to follow.

--- .github/actions/archlinux/entrypoint.sh
+++ .github/actions/archlinux/entrypoint.sh
@@ -8,26 +8,26 @@ od=$(pwd)
 chown -R build: .
 
 if [ -n "${GPG_FILE_PASSWORD}" ]; then
-openssl aes-256-cbc -d -a -pbkdf2 -in "${GPGKEY}" -pass "pass:${GPG_FILE_PASSWORD}" | sudo -u build gpg --import
-unset GPGKEY
-unset GPG_FILE_PASSWORD
+	openssl aes-256-cbc -d -a -pbkdf2 -in "${GPGKEY}" -pass "pass:${GPG_FILE_PASSWORD}" | sudo -u build gpg --import
+	unset GPGKEY
+	unset GPG_FILE_PASSWORD
 fi
 
 if [ -n "${GPG_KEY_PASSWORD}" ]; then
-    echo "allow-preset-passphrase" | sudo -u build tee -a /home/build/.gnupg/gpg-agent.conf
-    sudo -u build gpg-connect-agent reloadagent /bye
-    KGRIPS=$(sudo -u build gpg --list-secret-keys --with-colons --with-keygrip | awk -F: '$1 == "grp" {print $10}')
-    for KGRIP in $KGRIPS; do
-        echo "${GPG_KEY_PASSWORD}" | sudo -u build /usr/lib/gnupg/gpg-preset-passphrase --preset "${KGRIP}"
-    done
-    unset GPG_KEY_PASSWORD
+	echo "allow-preset-passphrase" | sudo -u build tee -a /home/build/.gnupg/gpg-agent.conf
+	sudo -u build gpg-connect-agent reloadagent /bye
+	KGRIPS=$(sudo -u build gpg --list-secret-keys --with-colons --with-keygrip | awk -F: '$1 == "grp" {print $10}')
+	for KGRIP in $KGRIPS; do
+		echo "${GPG_KEY_PASSWORD}" | sudo -u build /usr/lib/gnupg/gpg-preset-passphrase --preset "${KGRIP}"
+	done
+	unset GPG_KEY_PASSWORD
 fi
 
 cd "$1"
 shift
 
 if [ -d "_deps" ]; then
-    find _deps -name "*.pkg.tar.zst" -exec pacman -U --noconfirm {} +
+	find _deps -name "*.pkg.tar.zst" -exec pacman -U --noconfirm {} +
 fi
 
 sudo -u build --preserve-env=PACKAGER bash -c "$*"
